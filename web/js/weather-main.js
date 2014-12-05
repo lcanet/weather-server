23ab7b6d099a10ad3842774292d697e3830a9c83
lcanet <lc@lc6.net>
2014-12-05 19:46:29 +0100

    fix #10

--- web/js/weather-main.js
+++ web/js/weather-main.js
@@ -1,2 +1,9 @@
 angular.module('weatherDashboard', ['ngRoute', 'ngMaterial']);
 
+angular.module('weatherDashboard').run(['$timeout', function($timeout) {
+    $timeout(function(){
+        $('.preloader').fadeOut(300, function(){
+            $('.main-content').show();
+        });
+    }, 50);
+}]);
